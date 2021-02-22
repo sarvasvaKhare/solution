8e664cf3e72380d49e63a3ea1c867fcc2ba17049
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-02-22 17:50:23 +0530

    just silly cases

--- models/moderators.js
+++ models/moderators.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const moderator = mongoose.model('moderator',
 {   UID: {type:String,index:true,unique:true,required: true},
-    orgID: {type:Number,required: true,index:true},
+    orgId: {type:Number,required: true,index:true},
     email:{type:String,unique:true,required: true},
     access:{type:String,required: true}
 })
