53de1fb55f42f71bde59856e03b5f421633ca91c
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-23 23:37:40 +0530

    done

--- models/paymentinfo.js
+++ models/paymentinfo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const payinfoSchema = new mongoose.Schema({
-    OrgId:String,
+    orgId:String,
     google:{
         upiId: String,
         merchantName: String
