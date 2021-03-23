6a805b247056adfb41d3c48fae1fe1df050aca01
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-24 03:30:08 +0530

    lets test

--- index.js
+++ index.js
@@ -491,6 +491,7 @@ app.get('/feed',async (req,res)=>{
   res.status(200).send(posts)
 }else{
   posts = await OrgFeed.find({}).sort({created_at:-1})
+  console.log(posts[0])
   res.status(200).send(posts)
 }
 })
