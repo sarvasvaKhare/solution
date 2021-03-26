3bb6dd6d84125b24547952f184db7bc11bd52c99
sarvasvaKhare <f20190752p@pilani.bits-pilani.ac.in>
2021-03-26 20:15:08 +0530

    done

--- index.js
+++ index.js
@@ -422,7 +422,7 @@ app.post('/follow', async (req,res)=>{
     })
   }else{
     if(ticket.orgprofile.orgId==req.body.orgId){
-      res.status(400).send({"success":false})
+      res.status(200).send({"success":false})
     }else{
     name=ticket.orgprofile.orgName
     var conditions = {
