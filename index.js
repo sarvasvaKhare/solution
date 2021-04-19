6d7b2a8845b520e8c696b47d917585b71a5f6cda
Ayush Suman <ayush231suman@gmail.com>
2021-04-19 20:24:50 +0530

    Update Feed OrgImage

--- index.js
+++ index.js
@@ -631,7 +631,8 @@ app.get('/feed',async (req,res)=>{
   }
   for(var i=0;i<posts.length;i++){
     var n=posts[i].likes.length
-    posts[i].orgPhoto = await organisation.findOne({orgId:posts[i].orgId}).photo
+    var org = await organisation.findOne({orgId:posts[i].orgId})
+    posts[i].orgPhoto = org.photo
     for(var j=0;j<n;j++){
         if(posts[i].likes[j].userId==ID){
           posts[i].liked=true
