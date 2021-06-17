5c8f7e278086f6628bf2337f5690a0c39a8d4dea
Ayush Suman <ayush231suman@gmail.com>
2021-06-17 19:56:54 +0530

    Update applicants.js

--- models/applicants.js
+++ models/applicants.js
@@ -3,7 +3,10 @@ const applicantSchema = new mongoose.Schema({
     email:String,
     orgId:Number,
     Reason: String,
-    ModEmail: String
+    ModEmail: String,
+    number:String,
+    profilePic:String,
+    name:String
 })
 const applicant = mongoose.model('applicant',applicantSchema)
 module.exports = applicant
